12d8a3f1e0e2de4b617822b572107971d0a59ec7
Oleg Chunikhin <ochunikhin@eastbanctech.com>
2018-05-04 13:07:35 -0400

    Code updated

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello New World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
